96c29b6bdc6cb7803b3063f0240d0f2e4585d94f
waltermacias <oscarmacias191330@correo.itm.edu.co>
2021-09-01 00:06:40 -0500

    Add Entity taller

---
+++ tallerazure.Common/Models/Taller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace tallerazure.Common.Models
+{
+     public class Taller
+     {
+          public DateTime CreatedTime { get; set; }
+
+          public string NameEmployee { get; set; }
+
+          public bool IsCompleted { get; set; }
+     }
+}
---
+++ tallerazure.Function/Entities/TallerEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace tallerazure.Function.Entities
+{
+     public class TallerEntity : TableEntity
+     {
+          public DateTime CreatedTime { get; set; }
+
+          public string NameEmployee { get; set; }
+
+          public bool IsCompleted { get; set; }
+     }
+}
