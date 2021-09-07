046f53f30826a913663f57535d3e365a349b013e
waltermacias <oscarmacias191330@correo.itm.edu.co>
2021-09-06 23:36:07 -0500

    Test MockCloud

---
+++ tallerazure.test/Helpers/MockCloudTableTaller.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tallerazure.Test.Helpers
+{
+	public class MockCloudTableTaller : CloudTable
+	{
+		public MockCloudTableTaller(Uri tableAddress) : base(tableAddress)
+		{
+		}
+
+		public MockCloudTableTaller(StorageUri tableAddress, StorageCredentials credentials) : base
+			(tableAddress, credentials)
+		{
+		}
+
+		public MockCloudTableTaller(Uri tableAbsoluteUri, StorageCredentials credentials) : base
+			(tableAbsoluteUri, credentials)
+		{
+		}
+		public override async Task<TableResult> ExecuteAsync(TableOperation operation)
+		{
+			return await Task.FromResult(new TableResult
+			{
+				HttpStatusCode = 200,
+				Result = TestFactory.GetTodoEntity()
+			});
+		}
+	}
+}
